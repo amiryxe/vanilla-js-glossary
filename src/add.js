3875ba01e325105d2de5a6de93bf24bb1aa89bdb
Amir <nik.amirsa@gmail.com>
2020-04-23 23:22:44 +0430

    open/close modal

--- src/add.js
+++ src/add.js
@@ -1,5 +1,13 @@
 const addWord = () => {
-  return 'ADD FORM 2';
+  document.querySelector('.add-btn').addEventListener('click', () => {
+    document.querySelector('#addModal').classList.add('is-active');
+  });
+
+  document.querySelectorAll('.closeModal').forEach((item) => {
+    item.addEventListener('click', () => {
+      document.querySelector('#addModal').classList.remove('is-active');
+    });
+  });
 };
 
 export default addWord;
