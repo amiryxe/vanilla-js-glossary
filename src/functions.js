a6a9186a410685a74cd96fc90dee683d07848660
Amir <nik.amirsa@gmail.com>
2020-04-25 02:59:36 +0430

    improve cardTemplate

--- src/functions.js
+++ src/functions.js
@@ -21,30 +21,29 @@ export const saveWords = (words) => {
 
 // Card Template
 const cardTemplate = `
-  <div class="card" style="margin-bottom: 1rem">
-        <header class="card-header">
-          <p class="card-header-title"></p>
-          <span class="card-header-icon"></span>
-        </header>
-        <div class="card-content">
-          <div class="content"></div>
-        </div>
-        <footer class="card-footer">
-          <a href="#" class="card-footer-item">
-            <i class="fa fa-edit has-text-info" style="margin-left: 10px;"></i>
-            ویرایش
-          </a>
-          <a href="#" class="card-footer-item delete-btn">
-            <i
-              class="fa fa-trash has-text-danger"
-              style="margin-left: 10px;"
-            ></i>
-            پاک کردن
-          </a>
-        </footer>
-      </div>
-    </div>
-  `;
+<div class="card" style="margin-bottom: 1rem">
+  <header class="card-header">
+    <p class="card-header-title"></p>
+    <span class="card-header-icon"></span>
+  </header>
+  <div class="card-content">
+    <div class="content"></div>
+  </div>
+  <footer class="card-footer">
+    <a href="#" class="card-footer-item">
+      <i class="fa fa-edit has-text-info" style="margin-left: 10px;"></i>
+      ویرایش
+    </a>
+    <a href="#" class="card-footer-item delete-btn">
+      <i
+        class="fa fa-trash has-text-danger"
+        style="margin-left: 10px;"
+      ></i>
+      پاک کردن
+    </a>
+  </footer>
+</div>
+`;
 
 // Render Words
 export const renderWords = (words, filters) => {
